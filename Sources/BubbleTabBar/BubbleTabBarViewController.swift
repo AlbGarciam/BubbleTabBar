144ec32d5b875e116e26e448ae1058d777c402c1
Alberto Garcia <alb.garciam@gmail.com>
2020-07-27 19:24:29 +0200

    remove tabbar when willshow

--- Sources/BubbleTabBar/BubbleTabBarViewController.swift
+++ Sources/BubbleTabBar/BubbleTabBarViewController.swift
@@ -154,13 +154,13 @@ extension BubbleTabBarViewController: UINavigationControllerDelegate {
     public func navigationController(_ navigationController: UINavigationController,
                                      willShow viewController: UIViewController,
                                      animated: Bool) {
+        let barWasHidden = bottomConstraint?.constant != Constants.verticalPadding
         let shouldDisplayTabBar = viewController.hidesBottomBarWhenPushed
         !shouldDisplayTabBar ? showTabBar(animated: animated) : hideTabBar(animated: animated)
         guard let coordinator = navigationController.topViewController?.transitionCoordinator else { return }
         coordinator.notifyWhenInteractionChanges { [weak self] context in
             guard context.isCancelled else { return }
-            let shouldHide = navigationController.topViewController?.hidesBottomBarWhenPushed ?? false
-            !shouldHide ? self?.showTabBar(animated: animated) : self?.hideTabBar(animated: animated)
+            barWasHidden ? self?.hideTabBar(animated: animated) : self?.showTabBar(animated: animated)
         }
     }
 }
